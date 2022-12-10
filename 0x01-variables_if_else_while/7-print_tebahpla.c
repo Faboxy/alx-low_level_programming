71605a130e3de4061a5079dfebc588f4b078ab58
Faboxy <projectdiamonds@gmail.com>
2022-12-10 12:09:03 -0800

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry Point
+ * Description - A program that writes the alphabets in reverse order
+ * In lower case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while char c = 'z';
+	{
+		putchar (c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
