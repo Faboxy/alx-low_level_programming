490a12aff7e1e606c42cc15e1e880efe03ba4513
Faboxy <projectdiamonds@gmail.com>
2022-12-09 17:17:58 -0800

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,7 +17,7 @@ int main(void)
 	if (n < 0)
 		printf("%d is negative\n", n);
 	else if (n == 0)
-		printf("%d is zero", n);
+		printf("%d is zero\n", n);
 	else 
 		printf("%d is positive\n", n);
 	return (0);
