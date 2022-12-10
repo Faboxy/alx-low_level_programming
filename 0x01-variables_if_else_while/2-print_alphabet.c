7d01dd1625a46462c780c3db1b7672eced76c4bb
Faboxy <projectdiamonds@gmail.com>
2022-12-09 18:14:48 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+
+}
