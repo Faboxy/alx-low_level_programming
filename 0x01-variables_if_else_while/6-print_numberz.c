4aa9e3939f0689401c1ad1b987e7a4597970363b
Faboxy <projectdiamonds@gmail.com>
2022-12-10 11:50:39 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry Point
+ * Description - A program that writes all single digit base 10 numbers
+ * Starting from 0 without using char, printf and puts
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int nums = 0;
+
+	while (nums = '0' && nums <= '9')
+	{
+		putchar(nums);
+	}
+	putchar('\n');
+	return (0);
+}
