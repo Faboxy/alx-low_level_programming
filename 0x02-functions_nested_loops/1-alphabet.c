685f8f8eec26bdb26e8fd483eb9a7b2e976a0538
Faboxy <projectdiamonds@gmail.com>
2022-12-13 04:15:42 -0800

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
+
 /**
  * print alphabet - print all alphabet in lowercase
  */
 
 void print_alphabet(void);
 }
-	
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
