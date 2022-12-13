798108e565c6de5adbe6137d4c6221bcf9306062
Faboxy <projectdiamonds@gmail.com>
2022-12-13 11:37:56 -0800

    Task 4

---
+++ 4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - checks for char alphabet
+ * @c: the char to be checked
+ *
+ * Return: 1 if char is letter lower or upper. else return 0
+ */
+int_isalpha(int c)
+
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && (c <= 'Z'))
+			return (1);
+		else
+			return (0);
