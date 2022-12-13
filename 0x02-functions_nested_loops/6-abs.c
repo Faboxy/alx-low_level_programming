3111a90c9933fe4dcce269ce83d2ded233510e4d
Faboxy <projectdiamonds@gmail.com>
2022-12-13 13:25:38 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * -abs -  computes the absolute value of an integer
+ * @n: the int in question
+ *
+ * Return: the abs of the int.
+ */
+
+int _abs(int n)
+
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
