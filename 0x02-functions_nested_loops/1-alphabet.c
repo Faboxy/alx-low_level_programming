799687afbc010543130a2b763c84c90d4ccda2c0
Faboxy <projectdiamonds@gmail.com>
2022-12-13 03:40:41 -0800

    A program that prints all alphabets in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,13 @@
 /**
  * mian - Entry point
  * A program that writes all the alphabets in lowercase a-z
- * Return: 0
  */
 void print_alphabet(void);
 }
 	char x;
-	for(x = 'a'; x <= 'z'; x++)
+
+	for (x = 'a'; x <= 'z'; x++)
 		_putchar('n');
-		_putchar('\n');
-		return (0);
+
+	_putchar('\n');
 }
