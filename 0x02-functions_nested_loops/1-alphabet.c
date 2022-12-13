497e933852ae12db59e6993635bd9d55126ba322
Faboxy <projectdiamonds@gmail.com>
2022-12-13 03:29:39 -0800

    Task 1 prints all alphates in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mian - Entry point
+ * A program that writes all the alphabets in lowercase a-z
+ * Return: 0
+ */
+void print_alphabet(void);
+}
+	char x;
+	for(x = 'a'; x <= 'z'; x++)
+	putchar('n');
+	putchar('\n');
+	return (0);
+}
