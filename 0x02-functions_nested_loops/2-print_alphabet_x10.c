20243abc06960a074c1565b592aed36efdefda2f
Faboxy <projectdiamonds@gmail.com>
2022-12-13 05:04:56 -0800

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - A program that prints x10 of the alphabet in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char x;
+
+	while (count++ <= 9)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+	       		_putchar(x x10);
+
+		_putchar('\n');
+	}
+
+}
