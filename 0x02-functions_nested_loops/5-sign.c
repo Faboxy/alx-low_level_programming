f6471245d48e162ec86a14905317ea6124890e11
Faboxy <projectdiamonds@gmail.com>
2022-12-13 12:52:13 -0800

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: the num to be checked
+ *  Return: 1 and prints + and n is greater than zero,
+ *  rturns 0 and prints 0 if n is zero
+ *  returns 1 and prints - if n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
