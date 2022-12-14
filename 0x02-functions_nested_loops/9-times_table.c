3c3e9b52ccce9c50d6be0e416d1b9327605aaf0a
Faboxy <projectdiamonds@gmail.com>
2022-12-13 18:23:23 -0800

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,9 +11,9 @@ void times_table(void)
 	int b;
 	int c;
 
-	for (a = o; a <= 9; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for b = 0; b <= 9; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			c = (a * b);
 			if ((c / 10) == 0)
