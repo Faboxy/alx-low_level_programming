78df072e356ad58dd8163471fbec9b9b1f3bfaaa
Faboxy <projectdiamonds@gmail.com>
2022-12-15 01:08:50 -0800

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - prints and two number and returns the result
+ * @i: print int i
+ * @k: print int k
+ * Return: always 0
+ */
+
+int add(int i, int k)
+{
+	return (i + k);
+}
