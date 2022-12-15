cbecdfcc49d7edf224dcfad55f774bcc268a47cd
Faboxy <projectdiamonds@gmail.com>
2022-12-15 08:20:26 -0800

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
-/** main - main block
- *
+/**
+ * main - main block
  * Return: always 0
  */
 
 int main(void)
 {
 	int i;
-	i = 0
+
+	i = 0;
+
 	positive_or_negative(i);
 
 	return (0);
