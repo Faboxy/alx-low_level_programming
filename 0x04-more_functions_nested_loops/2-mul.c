482131c2fba8699ed47840f5f90e856e8094ae62
Faboxy <projectdiamonds@gmail.com>
2022-12-18 12:36:41 -0800

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - function that does the multiplication
+* @a: first number
+* @b: second number
+* Return: 0
+*/
+
+int mul(int a, int b)
+{
+return (int a * int b);
+}
