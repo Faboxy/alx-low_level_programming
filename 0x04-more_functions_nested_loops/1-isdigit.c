3bfa7e6427fc98e81a23690ef301a9d2d9c1fdbe
Faboxy <projectdiamonds@gmail.com>
2022-12-18 12:08:20 -0800

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isdigit - checks for a digit
+* @c: the character to be checked
+* Return: 0
+*/
+
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 int _isupper(int c);
+int _isdigit(int c);
 int _putchar (char c);
 #endif /* MAIN_H */
