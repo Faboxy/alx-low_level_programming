f7d341b0fa1cd69db58cfc15be7817936887dda8
Faboxy <projectdiamonds@gmail.com>
2022-12-20 10:47:05 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _strlen - Entry point
+* Description: prints the lenght of a string
+* @s - lenght of characters to be printed
+* Return: i
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++1)
+	{
+		;
+	}
+	return (i);
+}
