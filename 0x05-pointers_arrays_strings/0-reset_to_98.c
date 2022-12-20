ce08dc462bc8f10b76f9bd2a03858cd97d2a738b
Faboxy <projectdiamonds@gmail.com>
2022-12-20 09:58:57 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* reset_to_98 - Entry point
+* Description: takes a pointer to an int and updates the value it points to 98
+* @n: checked
+* Return: 0
+*/
+void reset_to_98(int *n);
+{
+	int *ptr;
+
+	ptr = n;
+	*ptr = 98;
+}
