194ed262511df200d1fd582d458e048e32771280
Faboxy <projectdiamonds@gmail.com>
2022-12-20 17:57:42 -0800

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _puts - Entry point
+* @str: prints to stdout with a new line
+* Return: 0
+*/
+
+void _puts(char *str)
+{
+if (str)
+{
+while (*str)
+_putchar(*str++);
+}
+}
+_putchar('\n');
+}
