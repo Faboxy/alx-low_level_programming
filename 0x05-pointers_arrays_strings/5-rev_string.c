89efd9896624259cb0c7b6505ecc16e5e5bdc004
Faboxy <projectdiamonds@gmail.com>
2022-12-20 18:53:27 -0800

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* rev_string - reverses a string
+* @s: checked
+* Return: nothing
+*/
+
+void rev_string(char *s)
+{
+char *t = s;
+char n[1000];
+short c = 0;
+
+while (*s != '\0')
+{
+n[c] = *s;
+s++;
+c++;
+}
+c = 0;
+
+while (s > t)
+{
+s--;
+*s = n[c];
+c++;
+}
+}
