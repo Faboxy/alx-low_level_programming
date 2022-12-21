e0b1ec3faefc4705c7a3c21872d823dc5a1bf97c
Faboxy <projectdiamonds@gmail.com>
2022-12-20 18:22:37 -0800

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
-* _puts - Entry point
-* @str: prints to stdout with a new line
+* _puts - prints to stdout with a new line
+* @str: checked
 * Return: 0
 */
 
 void _puts(char *str)
 {
 
-if (*str)
+if (str)
 {
 while (*str)
 {
