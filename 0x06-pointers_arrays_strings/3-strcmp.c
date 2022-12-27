62de0f5f60c161905f8bbfde26d6cbb23e12f57d
Faboxy <projectdiamonds@gmail.com>
2022-12-27 09:20:51 -0800

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,7 +3,10 @@
 * _strcmp - compares two trings
 * @s1: string
 * @s2: second string
-* Return: void
+* Return: 
+* 	returns zero if s1 == s2
+* 	returns negative number if s1 < s2
+* 	returns positive number if s1 > s2
 */
 int _strcmp(char *s1, char *s2)
 {
