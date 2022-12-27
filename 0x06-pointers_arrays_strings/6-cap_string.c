e5b57361e2e4586cfc627ff6fac974e8d459adb6
Faboxy <projectdiamonds@gmail.com>
2022-12-27 12:16:14 -0800

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,6 +17,7 @@ if (str[index - 1] == ' ' ||
 str[index - 1] == '\t' ||
 str[index - 1] == '\n' ||
 str[index - 1] == ',' ||
+str[index - 1] == '.' ||
 str[index - 1] == ';' ||
 str[index - 1] == '!' ||
 str[index - 1] == '?' ||
