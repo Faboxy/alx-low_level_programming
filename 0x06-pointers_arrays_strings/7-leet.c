8348222305b613cc7c5abd9c16b1568cc593d828
Faboxy <projectdiamonds@gmail.com>
2022-12-28 00:04:09 -0800

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -17,7 +17,7 @@ for (index2 = 0; index2 <= 7; index2++)
 {
 if (str[index1] == leet[index2] ||
 str[index1] - 32 == leet[index2])
-str[index1] = index2 = '0';
+str[index1] = index2 + '0';
 }
 }
 return (str);
