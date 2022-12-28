b25904f8ae4215d2aef7a8730e19e4279718a8d8
Faboxy <projectdiamonds@gmail.com>
2022-12-28 02:00:56 -0800

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,30 +1,20 @@
 #include "main.h"
 
 /**
-* _strncat - concatenates two strings
-* using at most n-bytes from src
-* @dest: input value
-* @src: input value
-* @n: input value
-*
-* Return: dest
+* _strncat - concatenatyes two strings
+* @dest: string to be appended upon
+* @src: string to be completed at end of dest
+* @n: integer parameter to compare index to
+* Return: returns new concatenated string
 */
 char *_strncat(char *dest, char *src, int n)
 {
-int i, j;
+int index = 0, dest_len = 0;
 
-i = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
-j = 0;
-while (j < n && src[j] != '\0')
-{
-dest[i] = dest[j];
-i++;
-j++;
-}
-dest[i] = '\0';
+while (dest[index++])
+dest_len++;
+
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
 return (dest);
 }
