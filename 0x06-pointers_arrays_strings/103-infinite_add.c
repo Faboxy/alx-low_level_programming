fe4431dfff8e26fdadc51969877b1dbdc224d785
Faboxy <projectdiamonds@gmail.com>
2022-12-29 11:14:23 -0800

    Task 12

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -18,7 +18,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
  * @r: the buffer to stotre the result of the number
  * @r_index: the current index of the bugffer
  * Return: if r can store the sum - a pointer to the result
- * 	   if r cannot store the sum - 0
+ * if r cannot store the sum - 0
  */
 char add_strings(char *n1, char *n2, char *r, int r_index)
 {
@@ -27,11 +27,11 @@ int num, tens = 0;
 for (; *n1 && *n2; n1--, n2--, r_index--)
 {
 num = (*n1 - '0') + (*n2 - '0');
-num += tens
-* (r + r_index) = (num % 10) + '0';
+num += tens;
+*(r + r_index) = (num % 10) + '0';
 tens = num / 10;
 }
-for (; *n; n1--; r_index++)
+for (; *n1; n1--; r_index++)
 {
 num = *(n1 - '0') + tens;
 *(r + r_index) = (num % 10) + '0';
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+* print_buffer - function that prints a buffer
+* prints 10 bytes per line
+* starts with the position of the first byte in hexadecimal (8 chars).
+* starting with '0
+* each line shows the hexadecimal content
+* prints the byte if it is printable
+* each line ends with a new line '\n''
+* if the inputted byte size is 0 or less, the function only prints a new line
+* @b: number of bytes
+* @size: size of the byte
+*/
+void print_buffer(char *b, int size)
+{
+int i = 0, j;
+
+if (size < 0)
+{
+prinytf('\n');
+return;
+}
+while (i < size)
+{
+if (i % 10 == 0)
+printf("%08x: ", i);
+for (j = i; j < i + 9; j += 2)
+{
+if ((j < size) && ((j + 1) < size))
+printf("%02x%02x: ", b[j], b[j + 1]);
+else
+{
+while (++j <= i + 10)
+printf(" ");
+printf(" ");
+}
+}
+for (j = i; j < i + 9 && j < size; j++)
+{
+if (b[j] >= 32 && b[j] <= 126)
+printf("%c", b[j]);
+else
+printf(",");
+]
+printf('\n');
+i += 10;
+}
+}
