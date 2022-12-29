17a420aec1f1fe7c988681116748174a7a0f9c2b
Faboxy <projectdiamonds@gmail.com>
2022-12-29 15:34:54 -0800

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,18 +4,16 @@
 * puts2 - prints every character in a string starting from the first
 * @str: checked
 * Return 0
-*/ 
-
-void puts2(char *str)
+*/
+void	puts2(char *str)
 {
-int i;
+int i = 0;
 
-for (i = 0; str[i] != '\0'; ++i)
+while (str[i])
 {
 if (i % 2 == 0)
-{
 _putchar(str[i]);
+i++;
 }
 _putchar('\n');
-}
 }
