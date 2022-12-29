d8d369ea382b086bd5f5eebb8cb8b6fcadbd3689
Faboxy <projectdiamonds@gmail.com>
2022-12-29 15:38:04 -0800

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,21 +5,28 @@
 * @str: string parameter input
 * Return: Nothing
 */
+void	puts_half(char *str)
+{
+int n = _strlen(str);
 
-void puts_half(char *str)
-{
-int l, n;
-
-for (l = 0; str[l] != '\0'; ++l)
-if (1 % 2 == 0)
-{
-for (n = l / 2; str[n] != '\0'; ++n)
-_putchar(str[n]);
-}
-else
-{
-for (n = ((l - 1) / 2) + 1; str[n] != '\0'; ++n)
-_putchar(str[n]);
-}
+n = ((n % 2 == 0) ? n : n + 1) / 2;
+while (str[n])
+_putchar(str[n++]);
 _putchar('\n');
+}
+
+/**
+* _strlen - returns the length of the string
+* @s: the string
+*
+* Return: the length of the s string
+*/
+
+int	_strlen(char *s)
+{
+int i = 0;
+
+while (s[i])
+i++;
+return (i);
 }
