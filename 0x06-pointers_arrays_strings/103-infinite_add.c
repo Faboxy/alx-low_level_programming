bf6b3193a885508ce232934a8844664d86531fb6
Faboxy <projectdiamonds@gmail.com>
2022-12-29 02:26:44 -0800

    Task 11

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+* infinite_add - A function that adds two numbers
+* @n1: first number to be added
+* @n2: second number to be added
+* @r: stores the result
+* @size_r: size of buffer
+* Return: returns pointer to result
+*/
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+}
+/**
+ * add_strings - adds the numbers stored in two strings
+ * @n1: the string containing the first number
+ * @n2: the second string of number to be added to the first
+ * @r: the buffer to stotre the result of the number
+ * @r_index: the current index of the bugffer
+ * Return: if r can store the sum - a pointer to the result
+ * 	   if r cannot store the sum - 0
+ */
+char add_strings(char *n1, char *n2, char *r, int r_index)
+{
+int num, tens = 0;
+
+for (; *n1 && *n2; n1--, n2--, r_index--)
+{
+num = (*n1 - '0') + (*n2 - '0');
+num += tens
+* (r + r_index) = (num % 10) + '0';
+tens = num / 10;
+}
+for (; *n; n1--; r_index++)
+{
+num = *(n1 - '0') + tens;
+*(r + r_index) = (num % 10) + '0';
+tens = num / 10;
+}
+for (; *n2; n2--; r_index--)
+{
+num = (*n2 - '0') + tens;
+*(r + r_index) = (num % 10) + '0';
+tens = num / 10;
+}
+}
