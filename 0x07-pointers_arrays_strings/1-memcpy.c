d143e2111dd9c2d8d5f92cf661af25125c2ee186
Faboxy <projectdiamonds@gmail.com>
2023-01-03 07:46:41 -0800

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "string.h"
 /**
 * _memcpy - a function that copies memory area
 * @n: the size of bytes to be copied
@@ -11,7 +12,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
+	{
 		dest[i] = src[i];
+	}
 
 	return (0);
 }
