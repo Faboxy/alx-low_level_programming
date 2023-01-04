dbbc95b54e11961079d21415d2b76371e326d5a2
Faboxy <projectdiamonds@gmail.com>
2023-01-03 19:47:55 -0800

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,15 +9,13 @@
 char *_strchr(char *s, char c)
 {
 
-	while (*s)
-	{
-		if (*s != 'c')
-			s++;
+	unsigned int i = 0;
 
-		return (0);
-	}
-	if (*s == 'c')
-	{
-		return (s);
-	}
+	for ((; *s + i) != '\0'; i++)
+		if (*s(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
 }
+
