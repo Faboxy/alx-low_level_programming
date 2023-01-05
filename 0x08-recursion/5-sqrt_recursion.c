947abdff5cdbcd1b0ee54752854d706e42d666fe
Faboxy <projectdiamonds@gmail.com>
2023-01-05 13:45:12 -0800

    Task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+int sqrt_check(int g, int c)
+{
+	if (g * g == c)
+
+		return (g);
+	if (g * g > c)
+		return (-1);
+	return (sqrt_check(g + 1, c));
+}
+
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number
+ * @n: the number
+ * Return: returns -1 if the number does not have a natural sqrt
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (sqrt_check(1, n));
+}
