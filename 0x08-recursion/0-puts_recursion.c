adf2e33b8db60e33c0e7361dd7f9a8efbdb2da62
Faboxy <projectdiamonds@gmail.com>
2023-01-05 12:33:07 -0800

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_Putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
 		_putchar('\n');
-
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
 
