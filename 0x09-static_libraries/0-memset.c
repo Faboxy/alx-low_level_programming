e2fb80a432264606a76f2d3419c9984fce4bcf46
Faboxy <projectdiamonds@gmail.com>
2023-01-09 10:34:24 -0800

    Task 0

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,4 +1,18 @@
 #include "main.h"
+/**
+* _memset  - a function that fills the first n bytes of memory area
+* @n: size of the memory area to be filled
+* @s: a pointer to the memory area
+* @b: the constant byte to be used in the filling
+* Return: returns a pointer to the memory area s
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
