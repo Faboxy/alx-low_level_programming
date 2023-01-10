4c9a529a082f0f467dacba0bbf94fb5e2ae36a5f
Faboxy <projectdiamonds@gmail.com>
2023-01-10 13:25:52 -0800

    Task 0

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 int _putchar(char);
 char *create_array(unsigned int size, char c);
-Prototype: char *_strdup(char *str);
-Prototype: char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
-Prototype: void free_grid(int **grid, int height);
+void free_grid(int **grid, int height);
 #endif
