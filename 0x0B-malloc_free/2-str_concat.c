c08c4f9ca77d34f049da1e2e7296c31a05ca61a0
Faboxy <projectdiamonds@gmail.com>
2023-01-11 14:45:07 -0800

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,29 +8,40 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i, j, k, l;
-	char *s;
+	char *s3;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-		i = 0;
-	else
-	{
-	for (i = 0; s[i]; i++)
-	}
-	if (s2 == NULL)
-		j = 0;
-	else
-	{
-	for (j = 0; s2[j]; j++)
-	}
-	k = i + j + 1;
-	s = malloc(k * sizeof(char));
-	if (s == NULL)
+	while (s1 && s1[len1])
+		len1++;
+
+	while (s2 && s2[len2])
+		len2++;
+
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
 		return (NULL);
-	for (l = 0; l < i; l++)
-		s[i] = s1[l];
-	for (l = 0; l < j; l++)
-		s[l + i] - s2[l];
-	s[i + j] = '\0';
-	return (s);
+	i = 0;
+	j = 0;
+
+	if (s1)
+	{
+		while (i < len1)
+		{
+			s3[i] = s1[i];
+			i++;
+		}
+	}
+
+	if (s2)
+	{
+		while (i < (len1 + len2))
+		{
+		s3[i] = s2[j];
+		i++;
+		j++;
+		}
+	}
+	s3[i] = '\0';
+
+	return (s3);
 }
