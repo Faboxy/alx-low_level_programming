7b3b9a93335be9e029754611463a9e1720afdfbb
Faboxy <projectdiamonds@gmail.com>
2023-01-12 20:39:37 -0800

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,20 +4,20 @@
  * _calloc - function that allocatres memory to an array using malloc
  * @nmemb: an array
  * @size: bytes of array
- * Return NUll at failure and if nmeb or size is 0
+ * Return: NUll at failure and if nmeb or size is 0
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *p;
-	unsigned int i;
+char *p;
+unsigned int i;
 
 if (nmemb == 0 || size == 0)
-	return (NULL);
+return (NULL);
 p = malloc(nmemb * size);
 if (p == NULL)
-	return (NULL);
+return (NULL);
 for (i = 0; i < nmemb * size; i++)
-	p[i] = 0;
+p[i] = 0;
 
 return (0);
 }
