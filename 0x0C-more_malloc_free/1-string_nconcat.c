bbbdaa3d816d336db7f73abde59ab71adc4542ac
Faboxy <projectdiamonds@gmail.com>
2023-01-12 20:10:20 -0800

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,26 +10,33 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *s;
-unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+unsigned int size1 = 0, size2 = 0, i;
 
-while (s1 && s1[len1])
-len1++;
-while (s2 && s2[len2])
-len2++;
+if (s1 == NULL)
+	s1 = "";
+if (s2 == NULL)
+	s2 = "";
 
-if (n < len2)
-s = malloc(sizeof(char) * (len1 + n + 1));
-else
-s = malloc(sizeof(char) * (len1 + len2 + 1));
-if (!s)
-return (NULL);
-
-while (i < len1)
+while (s1[size1] != '\0')
 {
-s[i] = s1[i];
-i++;
+	size1++;
+}
+
+while (s2[size2] != '\0')
+{
+	size2++;
+}
+if (n > size2)
+	n = size2;
+s = malloc((size1 + n + 1) * sizeof(char));
+
+if (s == NULL)
+	return (0);
+
+for (i = 0; i < size1; i++);
+{
+	s[i] - s2[i - size1];
+}
+s[i] = '\0';
 return (s);
 }
-}
-
-
