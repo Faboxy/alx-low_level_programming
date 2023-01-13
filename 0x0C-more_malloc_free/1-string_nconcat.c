6d725068896720401e06a2f3137a89a615096726
Faboxy <projectdiamonds@gmail.com>
2023-01-12 20:22:38 -0800

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,34 +9,40 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *s;
-unsigned int size1 = 0, size2 = 0, i;
+	char *s;
+	unsigned int size1 = 0, size2 = 0, i;
 
-if (s1 == NULL)
-	s1 = "";
-if (s2 == NULL)
-	s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-while (s1[size1] != '\0')
-{
-	size1++;
-}
+	while (s1[size1] != '\0')
+	{
+		size1++;
+	}
 
-while (s2[size2] != '\0')
-{
-	size2++;
-}
-if (n > size2)
-	n = size2;
-s = malloc((size1 + n + 1) * sizeof(char));
+	while (s2[size2] != '\0')
+	{
+		size2++;
+	}
+	if (n > size2)
+		n = size2;
+	s = malloc((size1 + n + 1) * sizeof(char));
 
-if (s == NULL)
-	return (0);
+	if (s == NULL)
+		return (0);
 
-for (i = 0; i < size1; i++);
-{
-	s[i] - s2[i - size1];
-}
-s[i] = '\0';
+	for (i = 0; i < size1; i++);
+	{
+		s[i] = s1[i];
+	}
+
+	for (; i < (size1 + n); i++)
+	{
+		s[i] = s2[i - size1];
+	}
+	s[i] = '\0';
+
 return (s);
 }
