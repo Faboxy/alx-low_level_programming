ce3d131ff3e753d77300d8c4a6d03fe86ba4c4c2
Faboxy <projectdiamonds@gmail.com>
2023-01-15 11:49:11 -0800

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - program that multiplies two numbers
+ * assume the result can be stored in an integer
+ * if the program does not receive two arguments it should print an error
+ * @argc: the argument count
+ * @argv: the argument vector
+ */
+int main(int argc, char *argv[])
+{
+	int index, multiplication;
+
+	multiplication = 1;
+	if (argc < 3)
+	{
+	printf("Error\n");
+	return (1);
+	}
+	for (index = 1; index < argc; index++)
+	{
+		multiplication = multiplication * atoi(argv[index]);
+	}
+	printf("%d\n", multiplication);
+	return (0);
+}
+
+
