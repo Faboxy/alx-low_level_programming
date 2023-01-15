c539cd7f94120c2b20223cf0b0ebab20545487f2
Faboxy <projectdiamonds@gmail.com>
2023-01-15 11:26:01 -0800

    Task 1

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the number of arguments passed to it
+ *
+ * @argc: the argument count
+ * @argv: the argument vector
+ *
+ * Return: return status always 0
+ */
+int main(int argc, char *argv[])0
+{
+	printf("%d\n", args - 1);
+	(void)argv;
+
+	return (0);
+}
