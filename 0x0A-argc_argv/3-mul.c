2fe5c92576b4f7aea467913d7c1d426bbcf641aa
Faboxy <projectdiamonds@gmail.com>
2023-01-15 12:03:03 -0800

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 	}
 	for (index = 1; index < argc; index++)
 	{
-		multiplication = multiplication * atoi(argv[index]);
+		multiplication = multiplication * (argv[index]);
 	}
 	printf("%d\n", multiplication);
 	return (0);
