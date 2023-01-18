a8dc3a2696a8b83de247ba4e98040318b7619c4e
Faboxy <projectdiamonds@gmail.com>
2023-01-17 19:02:16 -0800

    Task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -20,7 +20,7 @@ void print_dog(struct dog *d)
 		printf("Age, %f\n", d->age);
 
 	if (d->owner == NULL)
-		printf(Öwner: (nil)\n");
+		printf("Owner: (nil)\n");
 	else
-		printf(Öwner: %s\n", d->owner);
+		printf("Owner: %s\n", d->owner);
 }
