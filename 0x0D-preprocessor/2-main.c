a0f43c38296a3ce7873f2f180f79e28adf1f5c2b
Faboxy <projectdiamonds@gmail.com>
2023-01-17 17:01:14 -0800

    Task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h
+/**
+ * main - prints the name of the file it was compile from
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("%s\n", _file_);
+	return (0);
+}
