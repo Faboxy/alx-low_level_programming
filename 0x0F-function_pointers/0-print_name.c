4341b70b773b4d7d2f65bab081ea36618cb992ec
Faboxy <projectdiamonds@gmail.com>
2023-01-18 18:53:39 -0800

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - function that prints a name
+ *@name: name to be printed
+ @f: pointer to the printing function
+ * Description - prints out a name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
+}
---
+++ 0x0F-function_pointers/_putchar.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/**
+ * main - function that prints a character
+ *@c: the charcter to be printed
+ * Return: return 1 on success, -1 on error and error is set appropriately
+ */
+
