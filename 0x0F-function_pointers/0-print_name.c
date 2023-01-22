9279375f8379bbfa0071620d8dd9b5f5be28044b
Faboxy <projectdiamonds@gmail.com>
2023-01-22 15:36:14 -0800

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stdlio.h>
+#include <stdio.h>
 
 /**
  * print_name - function that prints a name
@@ -8,7 +8,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
-		return;
-	f(name);
+	if (name && f)
+		f(name);
 }
