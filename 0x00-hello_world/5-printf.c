e70bc37a2a11621f370a7cc95c66eb0930c7b6ee
Faboxy <projectdiamonds@gmail.com>
2023-01-25 12:36:55 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
 /**
  * main - Entry Point
- * Description - A program that prints exactly
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 
 }
