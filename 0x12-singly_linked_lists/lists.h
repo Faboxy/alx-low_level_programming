b4dc38e764118a03f9e9965095cb1a2684a39e68
Faboxy <projectdiamonds@gmail.com>
2023-01-30 16:19:52 -0800

    Task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,18 +2,18 @@
 #define LISTS_H
 #include <stdlib.h>
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
+* struct list_s - singly linked list
+* @str: string - (malloc'ed string)
+* @len: length of the string
+* @next: points to the next node
+*
+* Description: singly linked list node structure
+*/
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
