43c6145c08b7f5c3515e4e1939d0d1e1b137e651
Faboxy <projectdiamonds@gmail.com>
2023-02-02 10:29:02 -0800

    Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of a listint_t
+ * @h: a pointer to the head of the list_t list
+ *
+ * Return: the number of nodes to the list_t list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (0);
+}
