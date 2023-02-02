7ad791ac984ad5fc30fca60591c4eeb7cab202d4
Faboxy <projectdiamonds@gmail.com>
2023-02-02 11:54:57 -0800

    Task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,7 +11,7 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
-	
+
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
@@ -19,6 +19,6 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new->next = *head;
 
 	*head = new;
-	freturn (new);
+	return (new);
 }
 
