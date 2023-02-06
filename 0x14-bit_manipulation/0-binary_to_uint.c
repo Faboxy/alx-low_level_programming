7ab851fe2d8ea61c32008982b5c0c19457302baf
Faboxy <projectdiamonds@gmail.com>
2023-02-06 02:55:26 -0800

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * binar_to_vint - converts a binary number to unsigned int
+ * binar_to_uint - converts a binary number to unsigned int
  * @b: string containing the binary number
  *
  * Return: the converted number
  */
-unsigned int binary_to_vint(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	int i;
 	unsigned int dec_val = 0;
