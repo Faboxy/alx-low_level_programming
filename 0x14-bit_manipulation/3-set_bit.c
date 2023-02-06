d32623c9dc39cc07cdb653253e5c98ae440e52c8
Faboxy <projectdiamonds@gmail.com>
2023-02-06 09:50:43 -0800

    Task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,10 +10,10 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int max = 0x01;
 
-	max << index;
+	max <<= index;
 	if (max == 0)
 		return (-1);
 
-	*n != max;
+	*n |= max;
 	return (1);
 }
