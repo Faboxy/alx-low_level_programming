7e5e784886f463fcb3919707d8c2d90cab80cda7
Faboxy <projectdiamonds@gmail.com>
2023-02-06 10:18:29 -0800

    Task 101

---
+++ 0x14-bit_manipulation/101-password.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdio.h>
+/**
+  * main - The entry point of C programs
+  * @void: The function takes no parameters
+  *
+  * Description: This program that prints all possible different
+  * combinations of three digits.
+  * Return: 0
+  */
+int main(void)
+{
+	int firstnum, secondnum, thirdnum;
+
+	firstnum = 48;
+	while (firstnum < 58)
+	{
+		secondnum = 49;
+		while (secondnum < 58)
+		{
+			thirdnum = 50;
+			while (thirdnum < 58)
+			{
+				if (thirdnum > secondnum && secondnum > firstnum)
+				{
+					putchar(firstnum);
+					putchar(secondnum);
+					putchar(thirdnum);
+					if (firstnum != 55 || secondnum != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				thirdnum++;
+			}
+			secondnum++;
+		}
+		firstnum++;
+	}
+	putchar('\n');
+	return (0);
+}
